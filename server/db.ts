83488fac6d21aa654d0d3d88591386aab0191cb8
jerwelnaquimen2 <41258282-jerwelnaquimen2@users.noreply.replit.com>
2025-05-24 11:18:13 +0000

    Restored to '81e8f972158e203f4929323cbc2acbdd95e15d8b'
    Replit-Restored-To: 81e8f972158e203f4929323cbc2acbdd95e15d8b

--- server/db.ts
+++ server/db.ts
@@ -10,9 +10,6 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 export const db = drizzle(pool, { schema });
